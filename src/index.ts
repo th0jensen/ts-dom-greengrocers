bb46107cd988890946bd9f47f88007fcee1a7a0a
Thomas Jensen <thomas.jensen_@outlook.com>
2024-06-05 23:25:17 +0200

    Spaced out the code and added readability

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { StoreItem, CartItem, state } from './state'
 
 function createItemImage(item: StoreItem) {
     const itemIconContainer = document.createElement('div')
+
     const itemIcon = document.createElement('img')
     itemIcon.src = `assets/icons/${item.id}.svg`
     itemIcon.alt = item.name
+
     itemIconContainer.appendChild(itemIcon)
     return itemIconContainer
 }
@@ -13,6 +15,7 @@ function createItemImage(item: StoreItem) {
 function createAddToCartButton(item: StoreItem) {
     const addToCartButton = document.createElement('button')
     addToCartButton.innerText = 'Add to cart'
+
     addToCartButton.addEventListener('click', () => {
         const matchingItem = state.cart.find((cartItem: CartItem) =>
             cartItem.id.match(item.id)
@@ -29,10 +32,9 @@ function createAddToCartButton(item: StoreItem) {
             state.cart.push(newItem)
             console.log(`INFO: ${item.name} added to cart`)
             renderCart()
-        } else {
-            console.error('ERROR: Failed to add item to cart')
         }
     })
+
     return addToCartButton
 }
 
@@ -51,6 +53,7 @@ function addButton(cart: CartItem) {
 function removeButton(cart: CartItem) {
     const removeButton = document.createElement('button')
     removeButton.setAttribute('class', 'quantity-btn remove-btn center')
+
     removeButton.addEventListener('click', () => {
         if (cart.quantity > 1) {
             cart.quantity -= 1
@@ -59,19 +62,23 @@ function removeButton(cart: CartItem) {
             state.cart.splice(state.cart.indexOf(cart), 1)
             console.log(`INFO: ${cart.name} removed from cart`)
         }
+
         renderCart()
     })
+
     return removeButton
 }
 
 // TODO (#6): Do the maths for the cart
 function calculateTotalAmount() {
     let totalAmount = 0
+
     for (let i = 0; i < state.cart.length; i++) {
         const cartItem = state.cart[i]
         totalAmount += cartItem.price * cartItem.quantity
         console.log(`INFO: Total amount: ${totalAmount}`)
     }
+
     return totalAmount.toFixed(2)
 }
 
@@ -79,58 +86,61 @@ function calculateTotalAmount() {
 function renderCart() {
     const cartItemList = document.querySelector('.cart--item-list')
 
-    if (cartItemList) {
-        cartItemList.innerHTML = ''
-
-        for (let i = 0; i < state.cart.length; i++) {
-            const cart = state.cart[i]
-            const cartItem = document.createElement('li')
-
-            const cartItemImage = document.createElement('img')
-            cartItemImage.src = `assets/icons/${cart.id}.svg`
-            cartItemImage.alt = cart.name
-            cartItem.appendChild(cartItemImage)
-
-            const cartItemName = document.createElement('p')
-            cartItemName.innerText =
-                cart.name[0].toUpperCase() + cart.name.slice(1)
-            cartItem.appendChild(cartItemName)
-
-            const itemCount = document.createElement('span')
-            itemCount.innerText = cart.quantity.toString()
-
-            cartItem.appendChild(removeButton(cart))
-            cartItem.appendChild(itemCount)
-            cartItem.appendChild(addButton(cart))
-            cartItemList.appendChild(cartItem)
-        }
-
-        const totalAmount = document.querySelector('.total-number')
-        if (totalAmount) {
-            totalAmount.innerHTML = `£${calculateTotalAmount()}`
-        } else {
-            console.error('ERROR: No total-number class found in DOM')
-        }
-    } else {
+    if (!cartItemList) {
         console.error('ERROR: No cart--item-list class found in DOM')
+        return
     }
+
+    cartItemList.innerHTML = ''
+
+    for (let i = 0; i < state.cart.length; i++) {
+        const cart = state.cart[i]
+        const cartItem = document.createElement('li')
+
+        const cartItemImage = document.createElement('img')
+        cartItemImage.src = `assets/icons/${cart.id}.svg`
+        cartItemImage.alt = cart.name
+        cartItem.appendChild(cartItemImage)
+
+        const cartItemName = document.createElement('p')
+        cartItemName.innerText = cart.name[0].toUpperCase() + cart.name.slice(1)
+        cartItem.appendChild(cartItemName)
+
+        const itemCount = document.createElement('span')
+        itemCount.innerText = cart.quantity.toString()
+
+        cartItem.appendChild(removeButton(cart))
+        cartItem.appendChild(itemCount)
+        cartItem.appendChild(addButton(cart))
+        cartItemList.appendChild(cartItem)
+    }
+
+    const totalAmount = document.querySelector('.total-number')
+
+    if (!totalAmount) {
+        console.error('ERROR: No total-number class found in DOM')
+        return
+    }
+    totalAmount.innerHTML = `£${calculateTotalAmount()}`
 }
 
 // TODO (#1): Render the initial state
 function render() {
     const storeItemList = document.querySelector('.store--item-list')
-    if (storeItemList) {
-        storeItemList.innerHTML = ''
 
-        for (let i = 0; i < state.items.length; i++) {
-            const item = state.items[i]
-            const listItem = document.createElement('li')
-            listItem.appendChild(createItemImage(item))
-            listItem.appendChild(createAddToCartButton(item))
-            storeItemList.appendChild(listItem)
-        }
-    } else {
+    if (!storeItemList) {
         console.error('ERROR: No store--item-list class found in DOM')
+        return
+    }
+
+    storeItemList.innerHTML = ''
+
+    for (let i = 0; i < state.items.length; i++) {
+        const item = state.items[i]
+        const listItem = document.createElement('li')
+        listItem.appendChild(createItemImage(item))
+        listItem.appendChild(createAddToCartButton(item))
+        storeItemList.appendChild(listItem)
     }
 }
 
