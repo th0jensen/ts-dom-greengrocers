b63a9ecf02281d22c816ff5ad1c6c64992a0bd2b
Thomas Jensen <thomas.jensen_@outlook.com>
2024-06-07 00:09:43 +0200

    Over-engineering the shit out of state

--- src/state.ts
+++ src/state.ts
@@ -2,89 +2,52 @@ export type StoreItem = {
     id: string
     name: string
     price: number
-    kind: Filter
+    kind: string
+    src: string
 }
 
-export type CartItem = {
-    id: string
-    name: string
-    price: number
+export type CartItem = StoreItem & {
     quantity: number
 }
-
-export type Filter = 'Fruit' | 'Berry' | 'Vegetable' | null
 
 type State = {
     items: StoreItem[]
     cart: CartItem[]
-    filter: Filter
+    filter: string | null
     sorted: Boolean
+    isAdmin: Boolean
+}
+
+function createItem(
+    id: string,
+    name: string,
+    price: number,
+    kind: string
+): StoreItem {
+    return {
+        id: id,
+        name: name,
+        price: price,
+        kind: kind,
+        src: `./assets/icons/${id}.svg`,
+    }
 }
 
 export const state: State = {
     items: [
-        {
-            id: '001-beetroot',
-            name: 'beetroot',
-            price: 0.35,
-            kind: 'Vegetable',
-        },
-        {
-            id: '002-carrot',
-            name: 'carrot',
-            price: 0.35,
-            kind: 'Vegetable',
-        },
-        {
-            id: '003-apple',
-            name: 'apple',
-            price: 0.35,
-            kind: 'Fruit',
-        },
-        {
-            id: '004-apricot',
-            name: 'apricot',
-            price: 0.35,
-            kind: 'Fruit',
-        },
-        {
-            id: '005-avocado',
-            name: 'avocado',
-            price: 0.35,
-            kind: 'Vegetable',
-        },
-        {
-            id: '006-bananas',
-            name: 'bananas',
-            price: 0.35,
-            kind: 'Fruit',
-        },
-        {
-            id: '007-bell-pepper',
-            name: 'bell pepper',
-            price: 0.35,
-            kind: 'Vegetable',
-        },
-        {
-            id: '008-berry',
-            name: 'berry',
-            price: 0.35,
-            kind: 'Berry',
-        },
-        {
-            id: '009-blueberry',
-            name: 'blueberry',
-            price: 0.35,
-            kind: 'Berry',
-        },
-        {
-            id: '010-eggplant',
-            name: 'eggplant',
-            price: 0.35,
-            kind: 'Vegetable',
-        },
+        createItem('001-beetroot', 'beetroot', 0.35, 'Vegetable'),
+        createItem('002-carrot', 'carrot', 0.35, 'Vegetable'),
+        createItem('003-apple', 'apple', 0.35, 'Fruit'),
+        createItem('004-apricot', 'apricot', 0.35, 'Fruit'),
+        createItem('005-avocado', 'avocado', 0.35, 'Vegetable'),
+        createItem('006-bananas', 'bananas', 0.35, 'Fruit'),
+        createItem('007-bell-pepper', 'bell pepper', 0.35, 'Vegetable'),
+        createItem('008-berry', 'berry', 0.35, 'Berry'),
+        createItem('009-blueberry', 'blueberry', 0.35, 'Berry'),
+        createItem('010-eggplant', 'eggplant', 0.35, 'Vegetable'),
     ],
     cart: [],
     filter: null,
     sorted: false,
+    isAdmin: false,
 }
